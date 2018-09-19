afd0d7c6f4ced40204487804480f6b4ae64e35f7
Shay Bergmann <shaybergmann@gmail.com>
2018-09-19 14:06:54 -0400

    👢 Remove package: example-package

--- default.nix
+++ default.nix
@@ -14,7 +14,6 @@
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
   consulate = pkgs.callPackage ./pkgs/consulate { };
   imgcat = pkgs.callPackage ./pkgs/imgcat { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
