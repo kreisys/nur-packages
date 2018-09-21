a4659da12c38aa55d195b9c6ec9ce49ae78533d3
Shay Bergmann <shaybergmann@gmail.com>
2018-09-21 10:33:45 -0400

    💄 Reformat package: img2ansi

--- pkgs/img2ansi/default.nix
+++ pkgs/img2ansi/default.nix
@@ -1,23 +1,27 @@
-{ stdenv, lib, buildGoPackage }:
+{ stdenv, lib, buildGoPackage, fetchFromGitHub }:
 
 let
-  rev = "1b5600c638db787130d33940b4ea4349dd5517d5";
-  goPackagePath = "github.com/lloiser/img2ansi";
-in buildGoPackage {
-  name = "img2ansi-1.0.0";
+  repo        = "img2ansi";
+  description = "Converts an image to ANSI (using SGR escape sequences)";
 
-  inherit goPackagePath;
+  # It's not actually version 1.0.0, it looks like an abandoned project with no versioned releases
+  owner   = "lloiser";
+  version = "1.0.0";
+  rev     = "1b5600c638db787130d33940b4ea4349dd5517d5";
+  sha256  = "1936ha6avvqjpq2ypl4aplvss3w3gblrf641yvnwinlfmzq9w9ig";
+  license = stdenv.lib.licenses.mit;
 
+in buildGoPackage rec {
+  name = "${repo}-${version}";
   goDeps = ./deps.nix;
+  goPackagePath = "github.com/${owner}/${repo}";
 
-  src = builtins.fetchGit {
-    url = git:// + goPackagePath;
-    inherit rev;
+  src = fetchFromGitHub {
+    inherit owner repo rev sha256;
   };
 
   meta = with stdenv.lib; {
-    description = "Converts an image to ANSI (using SGR escape sequences)";
+    inherit description license;
     homepage = https:// + goPackagePath;
-    license = licenses.mit;
   };
 }
