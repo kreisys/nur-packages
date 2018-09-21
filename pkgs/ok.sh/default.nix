944423f65cafc6306de1eef0c039334fd1a85165
Shay Bergmann <shaybergmann@gmail.com>
2018-09-21 12:13:12 -0400

    🛠 Fix package: ok.sh
    ... was wrongly using builtins.fetchTarball.

--- pkgs/ok.sh/default.nix
+++ pkgs/ok.sh/default.nix
@@ -1,16 +1,17 @@
-{ stdenv, jq }:
+{ stdenv, fetchurl, jq }:
 
 let version = "0.2.3"; in
 stdenv.mkDerivation {
   name = "ok.sh-${version}";
-  src = builtins.fetchTarball {
+
+  src = fetchurl {
     url = "https://github.com/whiteinge/ok.sh/archive/${version}.tar.gz";
-    sha256 = "10afpws7vika15yf5ja7ghr9p0i081wirb05hl6l6r8g0lgdmalj";
+    sha256 = "1xhzxxcgw9ylw3rdqjh0ivcgl74fn0z47zghlhvsqnjh4fksk8b7";
   };
 
   buildInputs = [ jq ];
   buildPhase = "true";
   installPhase = ''
-    install -D $src/ok.sh $out/bin/ok.sh
+    install -D ok.sh $out/bin/ok.sh
   '';
 }
