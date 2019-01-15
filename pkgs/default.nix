1c01b10e6b560239693ed8e5600dea235b71c102
Shay Bergmann <shaybergmann@gmail.com>
2019-01-14 21:35:20 -0500

    Unpeg hydra

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -7,19 +7,13 @@ let
     inherit (import ../lib { inherit pkgs; }) grid;
   };
 
-  hydra.lastWorking.nixpkgs = pkgs.fetchFromGitHub {
-    owner   = "NixOS";
-    repo    = "nixpkgs-channels";
-    rev     = "61c3169a0e17d789c566d5b241bfe309ce4a6275";
-    sha256  = "0qbycg7wkb71v20rchlkafrjfpbk2fnlvvbh3ai9pyfisci5wxvq";
-  };
-
 in
 {
   consulate = pkgs.callPackage ./consulate { };
 
   fishPlugins = pkgs.recurseIntoAttrs (pkgs.callPackages ./fish-plugins { });
 
+  hydra    = pkgs.callPackage ./hydra {};
   img2ansi = pkgs.callPackage ./img2ansi   { };
   krec2    = pkgs.callPackage ./krec2.nix  { inherit mkBashCli; };
   kretty   = pkgs.callPackage ./kretty     { inherit mkBashCli; };
@@ -27,7 +21,4 @@ in
   oksh     = pkgs.callPackage ./ok.sh      { };
   webhook  = pkgs.callPackage ./webhook    { };
   xinomorf = (pkgs.callPackage ./xinomorf  { }).cli;
-
-  hydra = let pkgs' = import hydra.lastWorking.nixpkgs { inherit (pkgs) system; };
-    in pkgs'.callPackage ./hydra {};
 }
