60e221790647e3fb033780e5b7beae1258f66213
Shay Bergmann <shaybergmann@gmail.com>
2019-02-14 19:32:36 -0500

    vgo2nix: use abbreviated commit hash for version
    instead of a static "git"

--- pkgs/vgo2nix/default.nix
+++ pkgs/vgo2nix/default.nix
@@ -2,20 +2,19 @@
 
 assert lib.versionAtLeast go.version "1.11";
 
+let
+  isCommitHash   = rev: (builtins.match "[a-f0-9]{40}" rev) != null;
+  trimCommitHash = builtins.substring 0 7;
+  pin            = lib.importJSON ./pin.json;
 
-buildGoPackage rec {
-  name          = "vgo2nix-${version}";
-  version       = "git";
-  goPackagePath = "github.com/adisbladis/vgo2nix";
-
+in buildGoPackage rec {
+  name              = "vgo2nix-${version}";
+  version           = if isCommitHash pin.rev then trimCommitHash pin.rev else pin.rev;
+  goPackagePath     = "github.com/adisbladis/vgo2nix";
   nativeBuildInputs = [ makeWrapper ];
-
-  src = fetchFromGitHub (lib.importJSON ./pin.json);
-
-  #goDeps = ./deps.nix;
-  goDeps = src + "/deps.nix";
-
-  allowGoReference = true;
+  src               = fetchFromGitHub pin;
+  goDeps            = src + "/deps.nix";
+  allowGoReference  = true;
 
   postInstall = with stdenv; let
     binPath = lib.makeBinPath [ nix-prefetch-git go ];
@@ -25,8 +24,8 @@ buildGoPackage rec {
 
   meta = with stdenv.lib; {
     description = "Convert go.mod files to nixpkgs buildGoPackage compatible deps.nix files";
-    homepage = https://github.com/adisbladis/vgo2nix;
-    license = licenses.mit;
+    homepage    = https://github.com/adisbladis/vgo2nix;
+    license     = licenses.mit;
     maintainers = with maintainers; [ adisbladis ];
   };
 
