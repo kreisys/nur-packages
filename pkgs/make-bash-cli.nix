748d65a1c463006d460c15bf76b23932431459c3
Shay Bergmann <shaybergmann@gmail.com>
2019-02-21 12:13:37 -0500

    mkBashCli: various fixes

--- pkgs/make-bash-cli.nix
+++ pkgs/make-bash-cli.nix
@@ -1,162 +1,192 @@
-{ lib, grid, stdenv, bash, writeText, runCommand }:
+{ lib, grid, stdenv, bash, shfmt, linkFarm, writeText, runCommand }:
 
-name: description: { env ? {}, options ? [], flags ? [] }: commandsFn: let
-
-  mkCommand = name: description: { aliases ? [], commands ? [], options ? [], flags ? [] }: code: rec {
-    inherit name commands description aliases code options flags;
+let
+  self = name: description: { arguments ? [], aliases ? [], env ? {}, options ? [], flags ? [] }: commandsFn: let
     command = name;
-  };
+    defaultFlags = [ (mkFlag "h" "help" "show help") ];
 
-  commands = commandsFn mkCommand;
+    mkOption = short: long: default: description: { inherit short long default description; };
+    mkFlag   = short: long:          description: { inherit short long         description; };
 
-  mkOption = short: long: default: description: { inherit short long default description; };
-  mkFlag   = short: long: description: { inherit short long description; };
+    mkGetOpts = { options ? [], flags ? [], ... }@c: let
+      flags'   = if builtins.isFunction   flags then   flags mkFlag   else   flags;
+      options' = if builtins.isFunction options then options mkOption else options;
+    in ''
+      # Setup defaults
+      ${lib.concatMapStrings ({ long, ... }: ''
+        ${lib.toUpper long}=false
+      '') (defaultFlags ++ flags')}
 
-  mkGetOpts = { options ? [], flags ? [] }: let
-    flags'   = if builtins.isFunction   flags then   flags mkFlag   else   flags;
-    options' = if builtins.isFunction options then options mkOption else options;
-  in ''
-    POSITIONAL=()
+      ${lib.concatMapStrings ({ long, default ? null, ...}: ''
+        ${lib.optionalString (default != null) ''
+          ${lib.toUpper long}="${default}"
+        ''}
+      '') options'}
 
-    # Setup defaults
-    ${lib.concatMapStrings ({ long, ... }: ''
-      ${lib.toUpper long}=false
-    '') flags'}
+      while [[ $# -gt 0 ]]
+      do
+      key="$1"
 
-    ${lib.concatMapStrings ({ long, default ? null, ...}: ''
-      ${lib.optionalString (default != null) ''
-        ${lib.toUpper long}="${default}"
-      ''}
-    '') options'}
-
-    while [[ $# -gt 0 ]]
-    do
-    key="$1"
-
-    case $key in
-      ${lib.concatMapStrings ({ short, long, ... }:
-      ''-${short} | --${long} )
-        ${lib.toUpper long}=true
-        shift
-        ;;
-        '') flags'}
-      ${lib.concatMapStrings ({ short, long, ... }:
-      ''-${short} | --${long} )
-        ${lib.toUpper long}="$2"
-        shift 2
-        ;;
-        '') options'}
-
-      *)    # unknown option
-        POSITIONAL+=("$1") # save it in an array for later
-        shift # past argument
-        ;;
-    esac
-    done
-    set -- "''${POSITIONAL[@]}" # restore positional parameters
-  '';
-
-  usage = writeText "${name}-usage.txt" (mkUsageText { inherit name description flags options commands; });
-
-  commandsUsage = runCommand "${name}-commands-usage" {} ''
-    mkdir -p $out
-    cd $_
-
-    ${lib.concatMapStrings ({ command, ... }@c: ''
-      cat <<EOF > ${command}
-      ${mkUsageText c}
-      EOF
-    '') commands}
-  '';
-
-  mkUsageText = { name, description, flags, options, commands, ... }: let
-    flags'   = if builtins.isFunction   flags then   flags mkFlag   else   flags;
-    options' = if builtins.isFunction options then options mkOption else options;
-  in ''
-    Usage: ${name} [options/flags] <command> [args]
-
-      ${description}
-
-  '' + (lib.optionalString (flags != []) ''
-    Flags:
-    ${grid.gridToStringLeft (map ({ short, long, description, ... }: [
-      "    "       # indentation
-      "--${long}"  # long form
-      "-${short}"  # short form
-      " |"         # separator
-      description  # description
-    ]) flags')}
-  '') + (lib.optionalString (options != []) ''
-    Options:
-    ${grid.gridToStringLeft (map ({ short, long, default, description, ... }: [
-      "    "       # indentation
-      "--${long}"  # long form
-      "-${short}"  # short form
-      " |"         # separator
-      description  # description
-      "   [ ${if default != null then "default: ${default}" else "required"} ]"
-    ]) options')}
-  '') + (lib.optionalString (commands != []) ''
-    Commands:
-    ${grid.gridToStringLeft (map ({ command, description, aliases, ... }: [
-      "    "                                               # indentation
-      (lib.concatStringsSep ", " ([ command ] ++ aliases)) # command name and aliases
-      " |"                                                 # separator
-      description                                          # description
-    ]) commands)}
-  '');
-
-in stdenv.mkDerivation ({
-  inherit name;
-  phases = [ "buildPhase" "checkPhase" ];
-
-  buildPhase = ''
-    binary=$out/bin/${name}
-    mkdir -p $(dirname $binary)
-
-    cat <<'EOF' > $binary
-    #!${bash}/bin/bash
-
-    set -eo pipefail
-
-    stderr() {
-      1>&2 echo -e "$@"
-    }
-
-    err() {
-      stderr "\e[31merror\e[0m:" "$@"
-    }
-
-    ${mkGetOpts { inherit options flags; }}
-
-    cmd=$1
-
-    if [[ -n $cmd ]]; then shift; fi
-
-    case $cmd in
-      ${lib.concatMapStrings ({ command, code, aliases, options, flags, ... }: ''
-        ${command}${lib.optionalString (aliases != []) "|${lib.concatStringsSep "|" aliases}"})
-          ${mkGetOpts { inherit options flags; }}
-          ${code}
+      case $key in
+        ${lib.concatMapStrings ({ short, long, ... }:
+        ''-${short} | --${long} )
+          ${lib.toUpper long}=true
+          shift
           ;;
-        '') commands}
-      help)
-        if [[ -z $1 ]]; then
-          cat ${usage}
-        else
-          cat ${commandsUsage}/$1
-        fi
-        ;;
-      *)
-        if [[ -n $cmd ]]; then
-          err "unknown command: $cmd"
-        fi
-
-        cat ${usage}
-        ;;
+          '') (defaultFlags ++ flags')}
+        ${lib.concatMapStrings ({ short, long, ... }:
+        ''-${short} | --${long} )
+          ${lib.toUpper long}="$2"
+          shift 2
+          ;;
+          '') options'}
+        *) break ;;
       esac
-    EOF
+      done
 
-    chmod +x $binary
+      # Assert all options are defined
+      ${lib.concatMapStrings ({ long, ...}: ''
+        if ! [[ -v ${lib.toUpper long} ]] && ! $HELP; then
+          err "required option '${long}' has not been set"
+          opts_failed=
+        fi
+      '') options'}
+
+      if [[ -v opts_failed ]] || $HELP; then
+        cat ${mkUsage c}
+        exit 1
+      fi
     '';
-} // env)
+
+    mkUsageText = { name, description, flags ? [], options ? [], commands ? [], ... }: let
+      flags'   = if builtins.isFunction   flags then   flags mkFlag   else   flags;
+      options' = if builtins.isFunction options then options mkOption else options;
+    in ''
+      Usage: ${name} [options/flags] <command> [args]
+
+        ${description}
+
+    '' + (lib.optionalString ((defaultFlags ++ flags') != []) ''
+      Flags:
+      ${grid.gridToStringLeft (map ({ short, long, description, ... }: [
+        "    "       # indentation
+        "--${long}"  # long form
+        "-${short}"  # short form
+        " |"         # separator
+        description  # description
+      ]) (defaultFlags ++ flags'))}
+    '') + (lib.optionalString (options != []) ''
+      Options:
+      ${grid.gridToStringLeft (map ({ short, long, default, description, ... }: [
+        "    "       # indentation
+        "--${long}"  # long form
+        "-${short}"  # short form
+        " |"         # separator
+        description  # description
+        "   [ ${if default != null then "default: ${default}" else "required"} ]"
+      ]) options')}
+    '') + (lib.optionalString (commands != [] && ! builtins.isString commands) ''
+      Commands:
+      ${grid.gridToStringLeft (map ({ command, description, aliases ? [], ... }: [
+        "    "                                               # indentation
+        (lib.concatStringsSep ", " ([ command ] ++ aliases)) # command name and aliases
+        " |"                                                 # separator
+        description                                          # description
+      ]) commands)}
+    '');
+
+    mkUsage = { name, ... }@c: writeText "${name}-usage.txt" (mkUsageText c);
+
+    mkCli = { name, description, flags, options, ... }@c: let 
+      mkCommand = name: description: { arguments ? [], aliases ? [], options ? [], flags ? [] }@c: code: c // {
+        inherit name commands description code;
+        command = name;
+      };
+
+      commands = if builtins.isFunction commandsFn then commandsFn mkCommand else commandsFn;
+
+      usage = mkUsage (c // { inherit commands; });
+
+      commandsUsage = linkFarm "${name}-commands-usage" (lib.flatten (map ({ aliases ? [], command, ... }@c: let
+        usage = mkUsage c;
+      in [{
+        name = command;
+        path = usage;
+      }] ++ (map (alias: {
+        name = alias;
+        path = usage;
+      }) aliases)) commands));
+
+    in ''
+      ${mkGetOpts c}
+
+      ${if builtins.isString commands then ''
+        ${commands}
+      '' else ''
+
+      cmd=$1
+
+      if [[ -n $cmd ]]; then shift; fi
+
+      case $cmd in
+        ${lib.concatMapStrings ({ name, code, aliases ? [], options ? [], flags ? [], ... }: ''
+          ${name}${lib.optionalString (aliases != []) "|${lib.concatStringsSep "|" aliases}"})
+            ${mkGetOpts c}
+            ${code}
+            ;;
+          '') commands}
+        help)
+          if [[ -z $1 ]]; then
+            cat ${usage}
+          elif [[ -f ${commandsUsage}/$1 ]]; then
+            cat ${commandsUsage}/$1
+          else
+            err "help: unknown command: $1"
+          fi
+          ;;
+        *)
+          if [[ -n $cmd ]]; then
+            err "unknown command: $cmd"
+          fi
+
+          cat ${usage}
+          ;;
+      esac
+      ''}
+    '';
+
+  in stdenv.mkDerivation ({
+    inherit name;
+
+    phases      = [ "buildPhase" "checkPhase" ];
+    buildInputs = [ shfmt ];
+
+    buildPhase = ''
+      mkdir -p $out/bin
+
+      cd $_
+
+      cat <<'EOF' | shfmt -i 2 > ${name}
+      #!${bash}/bin/bash
+
+      set -eo pipefail
+
+      stderr() {
+        1>&2 echo -e "$@"
+      }
+
+      err() {
+        stderr "\e[31merror\e[0m:" "$@"
+      }
+
+      ${mkCli { inherit name description options flags; } }
+
+      EOF
+
+      chmod +x ${name}
+      ${lib.concatMapStrings (alias: ''
+        ln -s ${name} ${alias}
+      '') aliases}
+    '';
+    } // env);
+  in self
