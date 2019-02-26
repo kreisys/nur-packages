22455d1e5121a4ad1f72a993ca81bdd5a3c78896
Shay Bergmann <shaybergmann@gmail.com>
2019-02-26 16:19:56 -0500

    kurl: add command to fetch a page of commit hashes
    from the github api

--- pkgs/kreiscripts/kurl/default.nix
+++ pkgs/kreiscripts/kurl/default.nix
@@ -1,4 +1,4 @@
-{ mkBashCli, callPackage }:
+{ mkBashCli, jq }:
 
 mkBashCli "kurl" "a can of curls... just some interactions with REST APIs and such that I might need again in the future." {} (mkCmd: [
   (mkCmd "godaddy" "Useful interactions with the GoDaddy API" { aliases = [ "gd" ]; } [
@@ -26,6 +26,18 @@ mkBashCli "kurl" "a can of curls... just some interactions with REST APIs and su
       else
         curl -X GET -H "Content-Type: application/json" -H "Authorization: sso-key $(security find-generic-password -a default -s godaddy-api -w)" "https://api.godaddy.com/v1/domains"
       fi
+    '')
+  ])
+
+  (mkCmd "github" "Useful interactions with the GoDaddy API" { aliases = [ "gh" ]; } [
+    (mkCmd "commits" "list latest commit hashes" {
+      arguments = a: [ (a "repo" "the repository whose commits we want to list") ];
+      options   = o: [
+        (o "b" "branch" "master" "the branch whose commits we want to list")
+        (o "p" "page"   "1"      "github's API is paginated; we only get one page at a time")
+      ];
+    } ''
+      curl --silent "https://api.github.com/repos/$REPO/commits?sha=$BRANCH&page=$PAGE" | ${jq}/bin/jq -r .[].sha
     '')
   ])
 ])
