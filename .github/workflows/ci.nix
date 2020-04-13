ba3b90e75a9a44340251a98b726acb98251ff886
Shay Bergmann <shaybergmann@gmail.com>
2020-04-13 18:11:13 -0400

    Remove lorri as it's already in nixpkgs

--- .github/workflows/ci.nix
+++ .github/workflows/ci.nix
@@ -1,17 +1,15 @@
 with import <nixpkgs/lib>;
 let
+  # str -> [str]:
+  # Returns a list of the attribute paths of all non-broken packages
+  # compatible with 'system'.
   getBuildablesFor = system:
     (import ../../ci.nix { inherit system; }).buildables;
-  darwinBuildables = getBuildablesFor "x86_64-darwin";
-  linuxBuildables = getBuildablesFor "x86_64-linux";
 
   # str -> str -> str:
   # mkJobName "x86_64-darwin" "nested.package" -> "nested_package-x86_64-darwin"
   mkJobName = system: attrPath:
     "${replaceStrings [ "." ] [ "_" ] attrPath}-${system}";
-
-  mkDarwinJobName = mkJobName "x86_64-darwin";
-  mkLinuxJobName = mkJobName "x86_64-linux";
 
   mkJob = runs-on: attributes: {
     inherit runs-on;
@@ -29,18 +27,14 @@ let
     ];
   };
 
-  mkDarwinJob = mkJob "macos-latest";
-  mkLinuxJob = mkJob "ubuntu-latest";
+  mkJobs = system: jobTemplate:
+    pipe (getBuildablesFor system) [
+      ((flip genAttrs) id)
+      (mapAttrs' (n: v: nameValuePair (mkJobName system n) (jobTemplate v)))
+    ];
 
-  linuxJobs = pipe linuxBuildables [
-    ((flip genAttrs) id)
-    (mapAttrs' (n: v: nameValuePair (mkLinuxJobName n) (mkLinuxJob v)))
-  ];
-
-  darwinJobs = pipe darwinBuildables [
-    ((flip genAttrs) id)
-    (mapAttrs' (n: v: nameValuePair (mkDarwinJobName n) (mkDarwinJob v)))
-  ];
+  linuxJobs = mkJobs "x86_64-linux" (mkJob "ubuntu-latest");
+  darwinJobs = mkJobs "x86_64-darwin" (mkJob "macos-latest");
 
 in builtins.toJSON {
   name = "CI";
